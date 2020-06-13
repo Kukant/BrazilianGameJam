e6910de732a943c7eca4b478511989cecc9f6948
Filip Horky <Fezzzi@users.noreply.github.com>
2020-06-13 22:21:49 +0200

    Bum to tam

--- Assets/Scripts/hands_movement.cs
+++ Assets/Scripts/hands_movement.cs
@@ -6,16 +6,19 @@ public class hands_movement : MonoBehaviour {
     public Sprite HandFront0Sprite;
     public Sprite HandFront1Sprite;
     public Sprite HandFront2Sprite;
+    public Sprite HandFrontLaser;
     public Sprite HandBack0Sprite;
     public Sprite HandBack1Sprite;
     public Sprite HandBack2Sprite;
-    
-    
+    public Sprite HandBackLaser;
+
     private plex_movement.Direction direction;
     private SpriteRenderer frontHandSprite;
     private Animator frontHandAnimator;
     private SpriteRenderer backHandSprite;
     private Animator backHandAnimator;
+    private SpriteRenderer laserBallSpite;
+    private Animator laserBallAnimator;
     private bool idle;
 
     private plex_movement parentMovement;
@@ -32,59 +35,90 @@ public class hands_movement : MonoBehaviour {
         backHandSprite = GetComponentsInChildren<SpriteRenderer>()[1];
         backHandAnimator = GetComponentsInChildren<Animator>()[1];
         
+        laserBallSpite = GetComponentsInChildren<SpriteRenderer>()[2];
+        laserBallAnimator = GetComponentsInChildren<Animator>()[2];
+        laserBallAnimator.enabled = false;
+        
         frontHandSprite.sprite = HandFront0Sprite;
         idle = true;
     }
 
     // Update is called once per frame
     void Update() {
-        idle = parentAttacks.attacking;
+        idle = !parentAttacks.attacking;
         handsMovement();
     }
 
     private void handsMovement() {
         frontHandAnimator.enabled = false;
         backHandAnimator.enabled = false;
-        
-        switch (parentMovement.PlexDirection) {
-            case plex_movement.Direction.RIGHT:
-                frontHandSprite.sprite = HandFront2Sprite;
-                backHandSprite.sprite = HandBack2Sprite;
-                break;
-            case plex_movement.Direction.DOWN_R:
-                frontHandSprite.sprite = HandFront1Sprite;
-                backHandSprite.sprite = HandBack1Sprite;
-                break;
-            case plex_movement.Direction.DOWN:
-                frontHandSprite.sprite = HandFront1Sprite;
-                backHandSprite.sprite = HandBack1Sprite;
-                break;
-            case plex_movement.Direction.DOWN_L:
-                frontHandSprite.sprite = HandFront0Sprite;
-                backHandSprite.sprite = HandBack0Sprite;
-                break;
-            case plex_movement.Direction.LEFT:
-                frontHandSprite.sprite = HandFront0Sprite;
-                backHandSprite.sprite = HandBack0Sprite;
-                break;
-            case plex_movement.Direction.UP_L:
-                frontHandSprite.sprite = HandFront0Sprite;
-                backHandSprite.sprite = HandBack0Sprite;
-                break;
-            case plex_movement.Direction.UP:
-                frontHandSprite.sprite = HandFront1Sprite;
-                backHandSprite.sprite = HandBack1Sprite;
-                break;
-            case plex_movement.Direction.UP_R:
-                frontHandSprite.sprite = HandFront1Sprite;
-                backHandSprite.sprite = HandBack1Sprite;
-                break;
-            case plex_movement.Direction.NONE:
-                frontHandSprite.sprite = HandFront1Sprite;
-                backHandSprite.sprite = HandBack1Sprite;
-                frontHandAnimator.enabled = true;
-                backHandAnimator.enabled = true;
-                break;
+
+        if (idle) {
+            laserBallAnimator.enabled = false;
+            laserBallSpite.enabled = false;
+            
+            switch (parentMovement.PlexDirection) {
+                case plex_movement.Direction.RIGHT:
+                    frontHandSprite.sprite = HandFront2Sprite;
+                    backHandSprite.sprite = HandBack2Sprite;
+                    break;
+                case plex_movement.Direction.DOWN_R:
+                    frontHandSprite.sprite = HandFront1Sprite;
+                    backHandSprite.sprite = HandBack1Sprite;
+                    break;
+                case plex_movement.Direction.DOWN:
+                    frontHandSprite.sprite = HandFront1Sprite;
+                    backHandSprite.sprite = HandBack1Sprite;
+                    break;
+                case plex_movement.Direction.DOWN_L:
+                    frontHandSprite.sprite = HandFront0Sprite;
+                    backHandSprite.sprite = HandBack0Sprite;
+                    break;
+                case plex_movement.Direction.LEFT:
+                    frontHandSprite.sprite = HandFront0Sprite;
+                    backHandSprite.sprite = HandBack0Sprite;
+                    break;
+                case plex_movement.Direction.UP_L:
+                    frontHandSprite.sprite = HandFront0Sprite;
+                    backHandSprite.sprite = HandBack0Sprite;
+                    break;
+                case plex_movement.Direction.UP:
+                    frontHandSprite.sprite = HandFront1Sprite;
+                    backHandSprite.sprite = HandBack1Sprite;
+                    break;
+                case plex_movement.Direction.UP_R:
+                    frontHandSprite.sprite = HandFront1Sprite;
+                    backHandSprite.sprite = HandBack1Sprite;
+                    break;
+                case plex_movement.Direction.NONE:
+                    frontHandSprite.sprite = HandFront1Sprite;
+                    backHandSprite.sprite = HandBack1Sprite;
+                    frontHandAnimator.enabled = true;
+                    backHandAnimator.enabled = true;
+                    break;
+            }
+        } else {
+            switch (parentAttacks.ActiveAttack) {
+                case plex_atacks.ATTACK.LASER:
+                    if (!laserBallAnimator.enabled) {
+                        frontHandSprite.sprite = HandFrontLaser;
+                        backHandSprite.sprite = HandBackLaser;
+                        laserBallAnimator.enabled = true;
+                        laserBallSpite.enabled = true;
+                        laserBallAnimator.Rebind();
+                        laserBallAnimator.Play("Entry");
+                    }
+                    break;
+                case plex_atacks.ATTACK.MEELE:
+                    laserBallAnimator.enabled = false;
+                    laserBallSpite.enabled = false;
+                    break;
+                case plex_atacks.ATTACK.GUN:
+                    laserBallAnimator.enabled = false;
+                    laserBallSpite.enabled = false;
+                    break;
+            }
         }
     }
+    
 }
