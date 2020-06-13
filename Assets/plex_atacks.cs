33366970021e00a343b63731a00f04c153b24b87
Filip Horky <Fezzzi@users.noreply.github.com>
2020-06-13 15:00:21 +0200

    Add Plex prefab

---
+++ Assets/plex_atacks.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class plex_atacks : MonoBehaviour {
+    public ATTACK ActiveAttack = ATTACK.MEELE;
+    private int keyShifted = 0; // Just so that ctrl changing is more user-friendly
+    
+    public float MeeleRadius = 25f;
+    public float MeelePower = 10f;
+    public int MeeleCooldown = 50;
+    public int MeeleCooling = 0;
+    private CircleCollider2D meeleCollider;
+    
+    public float GunRadius = 10f;
+    public float GunPower = 10f;
+    public int GunCooldown = 5;
+    public int GunCooling = 0;
+    
+    public float LaserRadius = 10f;
+    public float LaserPower = 1f;
+    public int LaserCooldown = 0;
+    public int LaserCooling = 0;
+    private PolygonCollider2D laserCollider;
+    
+    public enum ATTACK {
+        MEELE,
+        GUN,
+        LASER,
+    }
+
+    // Start is called before the first frame update
+    void Start() {
+        meeleCollider = transform.GetComponentsInChildren<CircleCollider2D>()[0];
+        meeleCollider.radius = MeeleRadius;
+        meeleCollider.offset = new Vector2(MeeleRadius, 0);
+        meeleCollider.enabled = false;
+        
+        laserCollider = transform.GetComponentsInChildren<PolygonCollider2D>()[0];
+        laserCollider.enabled = false;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate() {
+        MeeleCooling = Math.Max(0, MeeleCooling - 1);
+        GunCooling = Math.Max(0, GunCooling - 1);
+        LaserCooling = Math.Max(0, LaserCooling - 1);
+
+        keyShifted = Math.Max(0, keyShifted - 1);
+        
+        meeleCollider.enabled = false;
+        laserCollider.enabled = false;
+
+        handleAttackSelection();
+        handleAttackUsage();
+    }
+
+    private void attack(float dx, float dy, float angleZ) {
+        transform.rotation = Quaternion.Euler(0, 0, angleZ);
+        switch (ActiveAttack) {
+            case ATTACK.MEELE:
+                if (MeeleCooling == 0) {
+                    MeeleCooling = MeeleCooldown;
+                    meeleCollider.enabled = true;
+                }
+                break;
+            case ATTACK.GUN: break;
+            case ATTACK.LASER:
+                if (LaserCooling == 0) {
+                    LaserCooling = LaserCooldown;
+                    laserCollider.enabled = true;
+                }
+                break;
+        }
+    }
+
+    private void handleAttackUsage() {
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.Keypad8)) {
+            if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.Keypad6)) {
+                attack(1, -1, 45);
+            } else if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.Keypad4)) {
+                attack(-1, -1, 135);
+            } else {
+                attack(0, -1 , 90);
+            }
+        } else if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.Keypad5)) {
+            if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.Keypad6)) {
+                attack(1, 1, -45);
+            } else if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.Keypad4)) {
+                attack(-1, 1, -135);
+            } else {
+                attack(0, 1, -90);
+            }
+        } else if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.Keypad4)) {
+            attack(-1, 0, 180);
+        } else if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.Keypad6)) {
+            attack(1, 0, 0);
+        }
+    }
+
+    private void handleAttackSelection() {
+        if (Input.GetKey(KeyCode.Alpha1)) {
+            ActiveAttack = ATTACK.MEELE;
+        } else if (Input.GetKey(KeyCode.Alpha2)) {
+            ActiveAttack = ATTACK.GUN;
+        } else if (Input.GetKey(KeyCode.Alpha3)) {
+            ActiveAttack = ATTACK.LASER;
+        } else if (Input.GetKey(KeyCode.LeftControl) && keyShifted == 0) {
+            ActiveAttack = (ATTACK)(((int)ActiveAttack + 1) % 3);
+            keyShifted = 10;
+        }
+    }
+}
---
+++ Assets/plex_movement.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using UnityEngine;
+
+public class plex_movement : MonoBehaviour {
+    public Direction PlexDirection = Direction.NONE;
+    public float RotationState = 0f;
+    public float RotationSpeed = .05f;
+    
+    public enum Direction {
+        UP,
+        UP_L,
+        LEFT,
+        DOWN_L,
+        DOWN,
+        DOWN_R,
+        RIGHT,
+        UP_R,
+        NONE,
+    };
+    
+    public float GetRotation(Direction d) {
+        switch (d) {
+            case Direction.UP: return .40f;
+            case Direction.UP_L: return .55f;
+            case Direction.LEFT: return .75f;
+            case Direction.DOWN_L: return .55f;
+            case Direction.DOWN: return .40f;
+            case Direction.DOWN_R: return .20f;
+            case Direction.RIGHT: return -.20f;
+            case Direction.UP_R: return .20f;
+        }
+        return 0f;
+    }
+
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        handleMovements();
+    }
+    
+    private void setRotationZ(float zAngle) {
+        Quaternion rot = transform.rotation;
+        transform.rotation = new Quaternion(rot.x, rot.y, zAngle, rot.w);
+    }
+
+    private void rotateTowards(Direction direction) {
+        PlexDirection = direction;
+        float endRotation = GetRotation(direction);
+        if (endRotation > 0) {
+            setRotationZ(Math.Min(transform.rotation.z + RotationSpeed, endRotation));
+        } else if (endRotation < 0) {
+            setRotationZ(Math.Max(transform.rotation.z - RotationSpeed, endRotation));
+        } else if (transform.rotation.z > 0) {
+            setRotationZ(Math.Max(transform.rotation.z - 2*RotationSpeed, endRotation));
+        } else if (transform.rotation.z < 0) {
+            setRotationZ(Math.Min(transform.rotation.z + 2*RotationSpeed, endRotation));
+        }
+    }
+
+    private void handleMovements() {
+        if (Input.GetKey(KeyCode.W)) {
+            if (Input.GetKey(KeyCode.D)) {
+                rotateTowards(Direction.UP_R);
+            } else if (Input.GetKey(KeyCode.A)) {
+                rotateTowards(Direction.UP_L);
+            } else {
+                rotateTowards(Direction.UP);
+            }
+        } else if (Input.GetKey(KeyCode.S)) {
+            if (Input.GetKey(KeyCode.D)) {
+                rotateTowards(Direction.DOWN_R);
+            } else if (Input.GetKey(KeyCode.A)) {
+                rotateTowards(Direction.DOWN_L);
+            } else {
+                rotateTowards(Direction.DOWN);
+            }
+        } else if (Input.GetKey(KeyCode.A)) {
+            rotateTowards(Direction.LEFT);
+        } else if (Input.GetKey(KeyCode.D)) {
+            rotateTowards(Direction.RIGHT);
+        } else {
+            rotateTowards(Direction.NONE);
+        }
+    }
+}
